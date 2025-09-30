2201723ae5fafc9912ec59650ac653934c1bc2c8
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-09-30 20:39:12 +1000

    Add docstrings to functions and change print_file() to read_file()

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,29 +9,35 @@ def main():
     write_name_to_file("name.txt", input_name)
 
     #2
-    print_file("name.txt")
+    read_in_name = read_file("name.txt")
+    print(f"Hi, {read_in_name}")
 
     #3
-    total = add_numbers_in_file("numbers.txt", 2)
-    print(total)
+    two_numbers_sum = add_numbers_in_file("numbers.txt", 2)
+    print(two_numbers_sum)
+
     #4
+    lines_to_add = count_lines_in_file("numbers.txt")
+    print(add_numbers_in_file("numbers.txt", lines_to_add))
 
 
 def write_name_to_file(filename, name):
+    """Write single line to file."""
     out_file_name = open(filename, "w")
     print(name, file=out_file_name)
     out_file_name.close()
 
 
-def print_file(filename):
-    """Prints the entire file contents"""
+def read_file(filename):
+    """Read the entire file contents."""
     in_file_name = open(filename, "r")
     file_contents = in_file_name.read()
-    print(file_contents)
     in_file_name.close()
+    return file_contents
 
 #3
 def add_numbers_in_file(filename, number_of_digits_to_add):
+    """Sum the numbers on each line of a file."""
     sum_numbers = 0.0
     with open(filename, "r") as in_file:
         for i in range(0, number_of_digits_to_add):
@@ -40,5 +46,12 @@ def add_numbers_in_file(filename, number_of_digits_to_add):
 
     return sum_numbers
 
+def count_lines_in_file(filename):
+    """Count lines in a file."""
+    number_of_lines = 0
+    with open(filename, 'r') as in_file:
+        for line in in_file:
+            number_of_lines += 1
+    return number_of_lines
 
 main()
