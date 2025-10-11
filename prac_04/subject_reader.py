0662c1317a00f7afb81c84753981fd9b622ca664
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-10-11 15:44:33 +1000

    Print loaded data in sentences

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,29 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    data = load_data_from_file(FILENAME)
+    for i in range(0, len(data)):
+        print(f"{data[i][0]} is taught by {data[i][1]} and has {data[i][2]} students")
 
 
-def load_data(filename=FILENAME):
+def load_data_from_file(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    data_points = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data_points.append(parts)
+        # print(parts)  # See if that worked
+        # print(data_points)
+        # print("----------")
+
     input_file.close()
+    return data_points
 
 
 main()
