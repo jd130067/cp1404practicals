87dfcfd18fdf85374824232da9df258021bf85c4
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-10-11 16:52:21 +1000

    Generate quick picks

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,29 @@
+import random
+
+NUMBERS_PER_PICK = 6
+MINIMUM_VALUE = 1
+MAXIMUM_VALUE = 45
+
+def main():
+    number_of_picks = int(input("How many quick picks?: "))
+    generate_quick_picks(number_of_picks)
+
+
+def generate_quick_picks(number_of_picks):
+    for i in range(number_of_picks):
+        quick_pick = []
+        while len(quick_pick) < NUMBERS_PER_PICK:
+            random_number = random.randint(MINIMUM_VALUE, MAXIMUM_VALUE)
+            if not(random_number in quick_pick):
+                quick_pick.append(random_number)
+
+        for k in range(len(quick_pick)):
+            minimum_index = i
+            for j in range(i+1, len(quick_pick)):
+                if quick_pick[j] < quick_pick[minimum_index]:
+                    minimum_index = j
+            quick_pick[i], quick_pick[minimum_index] = quick_pick[minimum_index], quick_pick[i]
+
+        print(quick_pick)
+
+main()
