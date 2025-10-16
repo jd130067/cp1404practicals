5886b6ea785f08309a8cd403ea7e8fd9074a68fd
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-10-16 14:32:53 +1000

    Add initial dictionary creation

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,3 +4,15 @@ Estimate: 40 minutes
 Actual:   .... minutes
 """
 
+word_collection = input("Text: ").strip().split()
+
+word_to_count = {}
+
+for word in word_collection:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+
+for word in word_to_count:
+    print(f"{word} : {word_to_count[word]}")
