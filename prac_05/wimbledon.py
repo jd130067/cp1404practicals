5ce7b63a3694d39148c819384724fb13144efb6b
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-10-18 16:38:55 +1000

    Refactor to use better naming

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,13 +3,12 @@ CP1404/CP5632 Practical
 Program to read and display Wimbledon champions and some information about them
 """
 
-# TODO fix the naming of champions and things oh my god it's so bad why did i call each row of data champions?
-
+FILENAME = "wimbledon.csv"
 
 def main():
     """Program to load Wimbledon data, process it and display it"""
-    tennis_champions_data = load_file_data("wimbledon.csv")
-    champions_to_count, countries_set = process_champions_data(tennis_champions_data)
+    mens_singles_wimbledon_history = load_file_data(FILENAME)
+    champions_to_count, countries_set = process_champions_data(mens_singles_wimbledon_history)
     display_information(champions_to_count, countries_set)
 
 
@@ -25,34 +24,30 @@ def display_information(champions_to_count, countries_set):
     print(country_list)
 
 
-def process_champions_data(champion_data):
+def process_champions_data(wimbledon_data_record):
     """Create dictionary with champion win counts and set of countries that have won."""
     # Counting champions is essentially a copy and paste of word occurrences.
     champion_to_count = {}
     countries_set = set()
-    for champion in champion_data:
+    for row_in_record in wimbledon_data_record:
+        countries_set.add(row_in_record[1])
         try:
-            champion_to_count[champion[2]] += 1
+            champion_to_count[row_in_record[2]] += 1
         except KeyError:
-            champion_to_count[champion[2]] = 1
-
-    for countries in champion_data:
-        countries_set.add(countries[1])
+            champion_to_count[row_in_record[2]] = 1
 
     return champion_to_count, countries_set
 
 
 def load_file_data(filename):
     """Create nested lists using file data."""
-    champions_data = []
+    file_data = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
         for line in in_file:
             test = line.strip().split(',')
-            champions_data.append(test)
-    return champions_data
-
-# set of countries
-# dictionary of winners and times won
+            file_data.append(test)
+    return file_data
 
 
 main()
