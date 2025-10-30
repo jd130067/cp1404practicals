d9706a6c2412932a919b3aeb970ec6c6e85550a2
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-10-30 14:02:01 +1000

    Add code from notebook to print dynamically typed languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,7 +4,9 @@ from programming_language import ProgrammingLanguage
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-languages = [python, ruby, visual_basic]
 
-if python.is_dynamic():
-    print("it totally works")
+languages = [python, ruby, visual_basic]
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
