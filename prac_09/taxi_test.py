a925f8c97077c358875b8eda7d90317a7a113204
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-11-20 13:21:35 +1000

    Add basic tests for taxi class

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,14 @@
+"""
+Program to test taxi class.
+"""
+
+from taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(my_taxi)
+print(f"Current Fare Price: ${my_taxi.get_fare()}")
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+print(f"Current Fare Price: ${my_taxi.get_fare()}")
