c818b156b3b139e120190c9c4b268a3353850aa6
jd130067 <oliver.higgins@my.jcu.edu.au>
2025-11-20 14:09:22 +1000

    Add and test silver service taxi

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,18 @@
+"""
+Basic testing for silver service taxi class.
+"""
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+normal_taxi = Taxi("Yellow", 100)
+fanci_taxi = SilverServiceTaxi("Silver", 100, 2)
+normal_taxi.drive(50)
+fanci_taxi.drive(50)
+
+print(normal_taxi)
+print(fanci_taxi)
+
+print(f"Normal Cost: {normal_taxi.get_fare()}")
+print(f"Fancy Cost: {fanci_taxi.get_fare()}")
+
